898c8fb65092731549c0cfbcce221532cb5e4369
yunseo323 <jokani3244833@gmail.com>
2021-10-12 20:08:42 +0900

    dotenv

--- codeFiles/learn-express/app.js
+++ codeFiles/learn-express/app.js
@@ -1,31 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
+
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => { //localhost:3000
-  //res.send('Hello, Express');// express에서는 res.write나 res.end 대신 res.send를 사용
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get('/about', (req, res) => { //localhost:3000/about
-  
-  res.send("hi about");
-});
-
-app.use((req,res,next) => {
-  try{
-    console.log("성공");
-  }catch(error){
-    next(error);
-  }
-})
-
-app.use((err, req, res, next) => { //미들웨어
-    console.error(err);
-    res.status(500).send("에러");
-  });
-  
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
-});
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
+}));
---
+++ codeFiles/learn-express/app_(1).js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => { //localhost:3000
+  //res.send('Hello, Express');// express에서는 res.write나 res.end 대신 res.send를 사용
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', (req, res) => { //localhost:3000/about
+  
+  res.send("hi about");
+});
+
+app.use((req,res,next) => {
+  try{
+    console.log("성공");
+  }catch(error){
+    next(error);
+  }
+})
+
+app.use((err, req, res, next) => { //미들웨어
+    console.error(err);
+    res.status(500).send("에러");
+  });
+  
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
